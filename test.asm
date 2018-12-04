7522bb1d6e13b2a4ee26543201cab673f117ae97
Melles <842595644@qq.com>
2018-12-04 17:36:42 +0800

    deal with merge

--- test.asm
+++ test.asm
@@ -11,6 +11,18 @@ beq $t1, $0, lable3
 lw $t1, 0($0)
 ori $t3, 11
 beq $t1, $t2, lable4
+#cal_r and load
+lw $t1, 0($0)
+addu $t2, $t1, $t2
+# cal_i and load
+lw $t2, 0($0)
+ori $t1, $t2, 34
+# load and load
+lw $t1, 0($0)
+lw $t2, 0($t1)
+# store and load
+lw $t1, 0($0)
+sw $t2, 0($t1)
 # jr and cal_r
 addu $t1, $t1, $ra
 jr $t1
@@ -24,18 +36,6 @@ jr $t1
 lw $t1, 0($0)
 addu $t2, $t3, $t2
 jr $t1
-#cal_r and load
-lw $t1, 0($0)
-addu $t2, $t1, $t2
-# cal_i and load
-lw $t2, 0($0)
-ori $t1, $t2, 34
-# load and load
-lw $t1, 0($0)
-lw $t2, 0($t1)
-# store and load
-lw $t1, 0($0)
-sw $t2, 0($t1)
 # beq and cal_r
 addu $t1, $t2, $t3
 ori $t3, $t2, 10
@@ -49,6 +49,15 @@ jal lable6
 addu $t1, $t2, $t2
 lable6:
 beq $ra, $t1, lable7
+# cal_r_ID and cal_r_EX and cal_r_MEM
+add $t1, $t2, $t3
+add $t2, $t3, $t4
+add $t3, $t1, $t2
+# cal_r_ID and cal_i_EX and cal_i_MEM
+ori $t1, $0, 20
+ori $t2, $0, 15
+add $t3, $t2, $t1
+# cal_r_ID and 
 # jr and cal_r
 addu $t1, $ra, $t2
 subu $t2, $t2, $t3
@@ -57,8 +66,9 @@ jr $t1
 ori $t1, 0x3008
 addu $t2, $t2, $t3
 jr $t1
-#jr and jal
+# jr and jal
 jal lable8
 addu $t1, $t2, $t1
 lable8:
 jr $ra
+# 
